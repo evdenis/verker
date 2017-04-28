3969f5c2c5cfc4fe9ad6dd251dcabbd1304f2ced
Denis Efremov <efremov@ispras.ru>
2017-04-28 15:47:58 +0300

    strchr verified

--- str/strchr.c
+++ str/strchr.c
@@ -2,8 +2,15 @@
 
 char *strchr(const char *s, int c)
 {
+	/*@ loop invariant valid_str(s);
+	    loop invariant \base_addr(s) == \base_addr(\at(s,Pre));
+	    loop invariant \at(s,Pre) <= s <= \at(s,Pre) + strlen(\at(s,Pre));
+	    loop invariant \forall integer i; 0 <= i < s - \at(s,Pre) ==> \at(s[i],Pre) != c;
+	    loop variant strlen(\at(s,Pre)) - (s - \at(s,Pre));
+	 */
 	for (; *s != (char)c; ++s)
 		if (*s == '\0')
 			return NULL;
+	//@ assert s <= \at(s,Pre) + strlen(\at(s,Pre));
 	return (char *)s;
 }
--- str/strchr.h
+++ str/strchr.h
@@ -2,7 +2,22 @@
 #define __STRCHR_H__
 
 #include "kernel_definitions.h"
+#include "strlen.h"
 
+/*@ requires valid_str(s);
+    requires c == ((char %) c);
+    assigns \nothing;
+    behavior not_exists:
+       assumes \forall integer i; 0 <= i <= strlen(s) ==> s[i] != c;
+       ensures \result == \null;
+    behavior exists:
+       assumes \exists integer i; 0 <= i <= strlen(s) && s[i] == c;
+       ensures \base_addr(\result) == \base_addr(s);
+       ensures s <= \result <= s + strlen(s);
+       ensures *\result == c;
+    complete behaviors;
+    disjoint behaviors;
+ */
 char *strchr(const char *s, int c);
 
 #endif // __STRCHR_H__
