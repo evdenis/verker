1a934ec651a40afb918d48c7c469467d6447dd0b
Denis Efremov <efremov@ispras.ru>
2017-04-28 15:58:40 +0300

    strchrnul verified

--- str/strchrnul.c
+++ str/strchrnul.c
@@ -2,6 +2,12 @@
 
 char *strchrnul(const char *s, int c)
 {
+	/*@ loop invariant valid_str(s);
+	    loop invariant \base_addr(s) == \base_addr(\at(s,Pre));
+	    loop invariant \at(s,Pre) <= s <= \at(s,Pre) + strlen(\at(s,Pre));
+	    loop invariant \forall integer i; 0 <= i < s - \at(s,Pre) ==> \at(s[i],Pre) != c;
+	    loop variant strlen(\at(s,Pre)) - (s - \at(s,Pre));
+	 */
 	while (*s && *s != (char)c)
 		s++;
 	return (char *)s;
--- str/strchrnul.h
+++ str/strchrnul.h
@@ -1,6 +1,23 @@
 #ifndef __STRCHRNUL_H__
 #define __STRCHRNUL_H__
 
+#include "strlen.h"
+
+/*@ requires valid_str(s);
+    requires c == ((char %) c);
+    assigns \nothing;
+    ensures \base_addr(\result) == \base_addr(s);
+    ensures s <= \result <= s + strlen(s);
+    behavior not_exists:
+       assumes \forall integer i; 0 <= i < strlen(s) ==> s[i] != c;
+       ensures \result == s + strlen(s);
+       ensures *\result == '\0';
+    behavior exists:
+       assumes \exists integer i; 0 <= i < strlen(s) && s[i] == c;
+       ensures *\result == c;
+    complete behaviors;
+    disjoint behaviors;
+ */
 char *strchrnul(const char *s, int c);
 
 #endif // __STRCHRNUL_H__
