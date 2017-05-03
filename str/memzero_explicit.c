7d809056fe222434a99b86282cad99f19ce376d9
Denis Efremov <efremov@ispras.ru>
2017-05-03 14:55:03 +0300

    remove memzero_explicit

--- str/memzero_explicit.c
+++
@@ -1,7 +0,0 @@
-#include "memzero_explicit.h"
-
-void memzero_explicit(void *s, size_t count)
-{
-	memset(s, 0, count);
-	barrier_data(s);
-}
--- str/memzero_explicit.h
+++
@@ -1,8 +0,0 @@
-#ifndef __MEMZERO_EXPLICIT_H__
-#define __MEMZERO_EXPLICIT_H__
-
-#include "kernel_definitions.h"
-
-void memzero_explicit(void *s, size_t count);
-
-#endif // __MEMZERO_EXPLICIT_H__
