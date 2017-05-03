891c702e4f57d859121e744a58cfad62a211a3ab
Denis Efremov <yefremov.denis@gmail.com>
2017-05-03 16:24:25 +0300

    memcmp update

--- str/memcmp.c
+++ str/memcmp.c
@@ -2,11 +2,22 @@
 
 /*@ requires \typeof(cs) <: \type(unsigned char *);
     requires \typeof(ct) <: \type(unsigned char *);
-    requires \valid(((unsigned char *)cs)+(0..count));
-    requires \valid(((unsigned char *)ct)+(0..count));
+    requires \valid(((unsigned char *)cs)+(0..count-1));
+    requires \valid(((unsigned char *)ct)+(0..count-1));
     requires \base_addr((unsigned char *)cs) == \base_addr((unsigned char *)ct) ^^
              \base_addr((unsigned char *)cs) != \base_addr((unsigned char *)ct);
     assigns \nothing;
+    behavior equal:
+       assumes \forall size_t i; 0 <= i < count ==> ((unsigned char *)cs)[i] == ((unsigned char *)ct)[i];
+       ensures \result == 0;
+    behavior diff:
+       assumes \exists size_t i; 0 <= i < count && ((unsigned char *)cs)[i] != ((unsigned char *)ct)[i];
+       ensures \exists size_t i; 0 <= i < count &&
+               (\forall size_t j; 0 <= j < i ==> ((unsigned char *)cs)[j] == ((unsigned char *)ct)[j]) &&
+               ((unsigned char *)cs)[i] != ((unsigned char *)ct)[i] &&
+               \result == ((unsigned char *)cs)[i] - ((unsigned char *)ct)[i];
+    complete behaviors;
+    disjoint behaviors;
  */
 __visible int memcmp(const void *cs, const void *ct, size_t count)
 {
@@ -21,7 +32,7 @@ __visible int memcmp(const void *cs, const void *ct, size_t count)
 	    loop invariant su1 - (unsigned char *)cs ==
 	                   su2 - (unsigned char *)ct ==
 	                   \at(count,Pre) - count;
-	    loop invariant \forall integer i; 0 <= i < su1 - (unsigned char *)cs ==>
+	    loop invariant \forall size_t i; 0 <= i < \at(count,Pre) - count ==>
 	                   ((unsigned char *)cs)[i] == ((unsigned char *)ct)[i];
 	    loop variant count;
 	 */
@@ -29,5 +40,7 @@ __visible int memcmp(const void *cs, const void *ct, size_t count)
 		if ((res = *su1 - *su2) != 0)
 			break;
 	//@ assert *su1 == *su2 ==> count == 0;
+	//@ assert count == 0 ==> res == 0;
+	//@ assert *su1 != *su2 ==> res != 0;
 	return res;
 }
