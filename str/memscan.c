f34ebbe334cdf80633ad6aaedea9c3ce2fd9a201
Denis Efremov <efremov@ispras.ru>
2017-05-03 16:22:52 +0300

    memscan fully verified

--- str/memscan.c
+++ str/memscan.c
@@ -4,11 +4,19 @@ void *memscan(void *addr, int c, size_t size)
 {
 	unsigned char *p = addr;
 
+	/*@ loop invariant 0 <= size <= \at(size,Pre);
+	    loop invariant p - (u8 *)addr == \at(size,Pre) - size;
+	    loop invariant \base_addr(p) == \base_addr((u8 *)addr);
+	    loop invariant (u8 *)addr <= p <= (u8 *)addr + \at(size,Pre);
+	    loop invariant \forall integer i; 0 <= i < \at(size,Pre) - size ==>
+	                   ((u8 *)addr)[i] != c;
+	    loop variant size;
+	 */
 	while (size) {
 		if (*p == c)
 			return (void *)p;
 		p++;
 		size--;
 	}
-  	return (void *)p;
+	return (void *)p;
 }
--- str/memscan.h
+++ str/memscan.h
@@ -3,6 +3,23 @@
 
 #include "kernel_definitions.h"
 
+/*@ requires \typeof(addr) <: \type(u8 *);
+    requires \valid((u8 *)addr+(0..size-1));
+    assigns \nothing;
+    ensures \base_addr(addr) == \base_addr(\result);
+    ensures addr <= \result <= addr + size;
+    behavior found:
+       assumes \exists integer i; 0 <= i < size && ((u8 *)addr)[i] == c;
+       ensures \exists integer i; 0 <= i < size &&
+               (\forall integer j; 0 <= j < i ==> ((u8 *)addr)[j] != c) &&
+               ((u8 *)addr)[i] == c &&
+               \result == addr + i;
+    behavior not_exists:
+       assumes \forall integer i; 0 <= i < size ==> ((u8 *)addr)[i] != c;
+       ensures \result == addr + size;
+    complete behaviors;
+    disjoint behaviors;
+ */
 void *memscan(void *addr, int c, size_t size);
 
 #endif // __MEMSCAN_H__
