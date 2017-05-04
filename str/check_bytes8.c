6c34c989e47c45b1608c505f34ca1823fb78228e
Denis Efremov <efremov@ispras.ru>
2017-05-04 16:43:10 +0300

    check_bytes verified

--- str/check_bytes8.c
+++ str/check_bytes8.c
@@ -1,29 +1,11 @@
 #include "check_bytes8.h"
 
-/*@ requires \valid(start+(0..bytes-1));
-    assigns \nothing;
-    behavior found:
-       assumes \exists integer i; 0 <= i < bytes && start[i] == value;
-       ensures \base_addr(start) == \base_addr(\result);
-       ensures start <= (u8 *)\result <= start + bytes;
-       ensures \exists integer i; 0 <= i < bytes &&
-               (\forall integer j; 0 <= j < i ==> start[j] != value) &&
-               start[i] == value &&
-               \result == start + i;
-    behavior not_exists:
-       assumes \forall integer i; 0 <= i < bytes ==> start[i] != value;
-       ensures \result == start + bytes;
-    complete behaviors;
-    disjoint behaviors;
- */
 static void *check_bytes8(const u8 *start, u8 value, unsigned int bytes)
 {
 	/*@ loop invariant 0 <= bytes <= \at(bytes,Pre);
-	    loop invariant \base_addr(\at(start,Pre)) == \base_addr(start);
 	    loop invariant \at(start,Pre) <= start <= \at(start,Pre) + \at(bytes,Pre);
-	    loop invariant \at(start,Pre) - start == \at(bytes,Pre) - bytes;
-	    loop invariant \forall integer i; 0 <= i < \at(bytes,Pre) - bytes ==>
-	                   \at(start,Pre)[i] != value;
+	    loop invariant start - \at(start,Pre) == \at(bytes,Pre) - bytes;
+	    loop invariant \forall u8 *i; \at(start,Pre) <= i < start ==> *i == value;
 	    loop variant bytes;
 	 */
 	while (bytes) {
@@ -32,5 +14,6 @@ static void *check_bytes8(const u8 *start, u8 value, unsigned int bytes)
 		start++;
 		bytes--;
 	}
+   //@ assert bytes == 0;
 	return NULL;
 }
--- str/check_bytes8.h
+++ str/check_bytes8.h
@@ -3,6 +3,19 @@
 
 #include "kernel_definitions.h"
 
+/*@ requires \valid(start+(0..bytes-1));
+    assigns \nothing;
+    behavior found:
+       assumes \exists u8 *i; start <= i < start + bytes && *i != value;
+       ensures start <= (u8 *)\result <= start + bytes;
+       ensures *((u8 *)\result) != value;
+       ensures \forall u8 *j; start <= j < (u8 *)\result ==> *j == value;
+    behavior not_exists:
+       assumes \forall u8 *i; start <= i < start + bytes ==> *i == value;
+       ensures \result == \null;
+    complete behaviors;
+    disjoint behaviors;
+ */
 static void *check_bytes8(const u8 *start, u8 value, unsigned int bytes);
 
 #endif // __CHECK_BYTES8_H__
