d9bb3a7b5fb0365f96e45929c3df52e70051f2cf
Denis Efremov <efremov@ispras.ru>
2017-05-05 20:17:30 +0300

    strcasecmp verified

--- str/strcasecmp.c
+++ str/strcasecmp.c
@@ -3,10 +3,29 @@
 int strcasecmp(const char *s1, const char *s2)
 {
 	int c1, c2;
+	//@ ghost char *os1 = s1;
+	//@ ghost char *os2 = s2;
 
+	/*@ loop invariant valid_str(s1) && valid_str(s2);
+	    loop invariant os1 <= s1 <= os1 + strlen(os1);
+	    loop invariant os2 <= s2 <= os2 + strlen(os2);
+	    loop invariant s1 - os1 == s2 - os2;
+	    loop invariant \forall integer i; 0 <= i < s1 - os1 ==>
+	                   tolower(os1[i]) == tolower(os2[i]);
+	    loop variant strlen(os1) - (s1 - os1);
+	*/
 	do {
 		c1 = tolower(*s1++);
 		c2 = tolower(*s2++);
 	} while (c1 == c2 && c1 != 0);
+	//@ ghost int res = c1 - c2;
+	//@ assert c1 == c2 ==> c1 == 0 && res == 0;
+	/*@ assert c1 != c2 ==>
+	    \exists integer i; 0 <= i <= strlen(os1) &&
+	            (\forall integer j; 0 <= j < i ==> tolower(os1[j]) == tolower(os2[j])) &&
+	            tolower(os1[i]) != tolower(os2[i]) &&
+               res == tolower(os1[i]) - tolower(os2[i]) &&
+               i == s1 - os1 - 1;
+	 */
 	return c1 - c2;
 }
--- str/strcasecmp.h
+++ str/strcasecmp.h
@@ -2,7 +2,25 @@
 #define __STRCASECMP_H__
 
 #include "kernel_definitions.h"
+#include "strlen.h"
+#include "../ctype.h"
 
+/*@ requires valid_str(s1);
+    requires valid_str(s2);
+    assigns \nothing;
+    behavior equal:
+       assumes \forall integer i; 0 <= i <= strlen(s1) ==> tolower(s1[i]) == tolower(s2[i]);
+       ensures \result == 0;
+    behavior not_equal:
+       assumes \exists integer i; 0 <= i <= strlen(s1) && tolower(s1[i]) != tolower(s2[i]);
+       ensures \result != 0;
+       ensures \exists integer i; 0 <= i <= strlen(s1) &&
+               (\forall integer j; 0 <= j < i ==> tolower(s1[j]) == tolower(s2[j])) &&
+               tolower(s1[i]) != tolower(s2[i]) &&
+               \result == tolower(s1[i]) - tolower(s2[i]);
+    complete behaviors;
+    disjoint behaviors;
+ */
 int strcasecmp(const char *s1, const char *s2);
 
 #endif // __STRCASECMP_H__
