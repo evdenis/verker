be6e7948878f8ad89b934ac43cd9d01a790ad50a
Denis Efremov <efremov@ispras.ru>
2017-05-05 20:20:08 +0300

    strcpy fix

--- str/strcpy.c
+++ str/strcpy.c
@@ -1,14 +1,21 @@
 #include "strcpy.h"
 
+
+/*@ requires valid_str(src);
+    requires \valid(dest+(0..strlen(src)));
+    requires \base_addr(dest) != \base_addr(src);
+    assigns dest[0..strlen(src)];
+    ensures valid_str(dest);
+    ensures \result == dest;
+    ensures \forall size_t i; i <= strlen(src) ==> \result[i] == src[i];
+ */
 char *strcpy(char *dest, const char *src)
 {
 	char *tmp = dest;
 	//@ ghost char *old_s = src;
 	//@ assert valid_str(old_s);
 
-	/*@ loop invariant \base_addr(src) == \base_addr(old_s);
-	    loop invariant \base_addr(dest) == \base_addr(tmp);
-	    loop invariant old_s <= src <= old_s + strlen(old_s);
+	/*@ loop invariant old_s <= src <= old_s + strlen(old_s);
 	    loop invariant tmp <= dest <= tmp + strlen(old_s);
 	    loop invariant valid_str(src);
 	    loop invariant dest - tmp == src - old_s;
--- str/strcpy.h
+++ str/strcpy.h
@@ -3,14 +3,6 @@
 
 #include "strlen.h"
 
-/*@ requires valid_str(src);
-    requires \valid(dest+(0..strlen(src)));
-    requires \base_addr(dest) != \base_addr(src);
-    assigns dest[0..strlen(src)];
-    ensures valid_str(dest);
-    ensures \result == dest;
-    ensures \forall size_t i; i <= strlen(src) ==> \result[i] == src[i];
- */
 char *strcpy(char *dest, const char *src);
 
 #endif // __STRCPY_H__
