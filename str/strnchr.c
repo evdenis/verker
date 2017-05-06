cc7f8a1448fbc07be0c0dd92228e3ce4550f3d75
Denis Efremov <yefremov.denis@gmail.com>
2017-05-06 15:47:49 +0300

    strnchr

--- str/strnchr.c
+++ str/strnchr.c
@@ -2,8 +2,18 @@
 
 char *strnchr(const char *s, size_t count, int c)
 {
-	for (; count-- && *s != '\0'; ++s)
-		if (*s == (char)c)
+	//@ ghost char *os = s;
+	//@ ghost size_t ocount = count;
+	/*@ loop invariant 0 <= count <= ocount;
+	    loop invariant os <= s <= os + strnlen(os, ocount);
+	    loop invariant s - os == ocount - count;
+	    loop invariant valid_strn(s, count);
+	    loop invariant strnlen(os, ocount) == s - os + strnlen(s, count);
+	    loop invariant \forall char *p; os <= p < s ==> *p != (char %) c;
+	    loop variant count;
+	 */
+	for (; count-- /*@%*/ && *s != '\0'; ++s)
+		if (*s == (char)/*@%*/c)
 			return (char *)s;
 	return NULL;
 }
--- str/strnchr.h
+++ str/strnchr.h
@@ -2,7 +2,21 @@
 #define __STRNCHR_H__
 
 #include "kernel_definitions.h"
+#include "strnlen.h"
 
+/*@ requires valid_strn(s, count);
+    assigns \nothing;
+    behavior exists:
+       assumes \exists char *p; s <= p < s + strnlen(s, count) && *p == (char %) c;
+       ensures s <= \result <= s + strnlen(s, count);
+       ensures *\result == (char %) c;
+       ensures \forall char *p; s <= p < \result ==> *p != (char %) c;
+    behavior not_exists:
+       assumes \forall char *p; s <= p < s + strnlen(s, count) ==> *p != (char %) c;
+       ensures \result == \null;
+    complete behaviors;
+    disjoint behaviors;
+ */
 char *strnchr(const char *s, size_t count, int c);
 
 #endif // __STRNCHR_H__
