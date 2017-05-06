b6c1ee30a6f4f706921b4b891020436ff4f498fc
Denis Efremov <yefremov.denis@gmail.com>
2017-05-06 23:29:00 +0300

    strcat draft update

--- str/strcat.c
+++ str/strcat.c
@@ -2,9 +2,15 @@
 
 /*@ requires valid_str(src);
     requires valid_str(dest);
-    requires \valid(dest+(0..strlen(dest)+strlen(src)-1));
-    assigns dest[strlen(dest)..strlen(dest)+strlen(src)-1];
+    requires \valid(dest+(0..strlen(dest)+strlen(src))); //-1
+    assigns dest[strlen(dest)..strlen(dest)+strlen(src)-1]; //-1
     ensures \result == dest;
+    ensures valid_str(\result);
+    ensures \forall integer i; 0 <= i < strlen(\old(dest)) ==>
+            dest[i] == \result[i];
+    ensures \forall integer i;
+            strlen(\old(dest)) <= i < strlen(\old(dest)) + strlen(src) ==>
+            src[i - strlen(\old(dest))] == \result[i];
  */
 char *strcat(char *dest, const char *src)
 {
@@ -13,30 +19,30 @@ char *strcat(char *dest, const char *src)
 	//@ assert valid_str(tmp);
 	//@ ghost size_t dest_len = strlen(tmp);
 
-	/*@ loop invariant \base_addr(dest) == \base_addr(tmp);
-	    loop invariant tmp <= dest <= tmp + strlen(tmp);
+	/*@ loop invariant tmp <= dest <= tmp + dest_len;
 	    loop invariant valid_str(dest);
-	    loop variant strlen(tmp) - (dest - tmp);
+	    loop variant dest_len - (dest - tmp);
 	 */
 	while (*dest)
 		dest++;
 	//@ assert *dest == '\0';
-	//@ assert dest == tmp + strlen(tmp);
-	//@ ghost Mid:
+	//@ assert dest == tmp + dest_len;
+	//@ ghost char *osrc = src;
+	//@ ghost char *mdest = dest;
 
-	/*@ loop invariant \base_addr(dest) == \base_addr(tmp);
-	    loop invariant \base_addr(src) == \base_addr(\at(src,Pre));
-	    loop invariant \at(src,Pre) <= src <= \at(src,Pre) + strlen(\at(src,Pre));
-	    loop invariant \at(dest,Mid) <= dest <= \at(dest,Mid) + strlen(\at(src,Pre));
-	    loop invariant src - \at(src,Pre) == dest - \at(dest,Mid);
+	/*@ loop invariant osrc <= src <= osrc + strlen(osrc);
+	    loop invariant mdest <= dest <= mdest + strlen(osrc);
+	    loop invariant src - osrc == dest - mdest;
 	    loop invariant valid_str(src);
-	    loop invariant \forall integer i; 0 <= i < src - \at(src,Pre) ==> tmp[dest_len + i] == \at(src,Pre)[i];
-	    loop assigns \at(dest,Mid)[0..strlen(src)];
-	    loop variant strlen(\at(src,Pre)) - (src - \at(src,Pre));
+	    loop invariant \forall integer i; 0 <= i < src - osrc ==>
+	                   mdest[i] == osrc[i];
+	    loop assigns mdest[0..strlen(osrc)];
+	    loop variant strlen(osrc) - (src - osrc);
 	 */
 	while ((*dest++ = *src++) != '\0')
 		;
 	//@ assert dest[-1] == '\0' && src[-1] == '\0';
-	//@ assert valid_str(tmp);
+	//@ assert dest - 1 == tmp + dest_len + strlen(osrc);
+	//@ assert tmp == \at(dest,Pre);
 	return tmp;
 }
