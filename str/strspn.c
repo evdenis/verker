bf23146f898a45ad2ce77ec5318420db8f5c37f3
Denis Efremov <yefremov.denis@gmail.com>
2017-05-07 13:54:05 +0300

    strspn verified

--- str/strspn.c
+++ str/strspn.c
@@ -6,7 +6,18 @@ size_t strspn(const char *s, const char *accept)
 	const char *a;
 	size_t count = 0;
 
+	/*@ loop invariant s <= p <= s + strlen(s);
+	    loop invariant 0 <= count <= strlen(s);
+	    loop invariant count == p - s;
+	    loop invariant \forall char *c; s <= c < p ==>
+	                   (\exists char *t; accept <= t < accept + strlen(accept) && *c == *t);
+	    loop variant strlen(s) - (p - s);
+	 */
 	for (p = s; *p != '\0'; ++p) {
+		/*@ loop invariant accept <= a <= accept + strlen(accept);
+		    loop invariant \forall char *c; accept <= c < a ==> *c != *p;
+		    loop variant strlen(accept) - (a - accept);
+		 */
 		for (a = accept; *a != '\0'; ++a) {
 			if (*p == *a)
 				break;
--- str/strspn.h
+++ str/strspn.h
@@ -2,7 +2,16 @@
 #define __STRSPN_H__
 
 #include "kernel_definitions.h"
+#include "strlen.h"
 
+/*@ requires valid_str(s);
+    requires valid_str(accept);
+    assigns \nothing;
+    ensures 0 <= \result <= strlen(s);
+    ensures \forall char *t; accept <= t < accept + strlen(accept) ==> s[\result] != *t;
+    ensures \forall char *p; s <= p < s + \result ==>
+            (\exists char *t; accept <= t < accept + strlen(accept) && *p == *t);
+ */
 size_t strspn(const char *s, const char *accept);
 
 #endif // __STRSPN_H__
