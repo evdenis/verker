56889361468116d8503adf146ea6c8b33673b4a0
Denis Efremov <yefremov.denis@gmail.com>
2017-05-07 21:03:54 +0300

    strcspn verified

--- str/strcspn.c
+++ str/strcspn.c
@@ -6,7 +6,19 @@ size_t strcspn(const char *s, const char *reject)
 	const char *r;
 	size_t count = 0;
 
+	/*@ loop invariant s <= p <= s + strlen(s);
+	    loop invariant 0 <= count <= strlen(s);
+	    loop invariant count == p - s;
+	    loop invariant \forall char *c, *t;
+	                   s <= c < p && reject <= t < reject + strlen(reject) ==>
+	                   *c != *t;
+	    loop variant strlen(s) - (p - s);
+	 */
 	for (p = s; *p != '\0'; ++p) {
+		/*@ loop invariant reject <= r <= reject + strlen(reject);
+		    loop invariant \forall char *c; reject <= c < r ==> *c != *p;
+		    loop variant strlen(reject) - (r - reject);
+		 */
 		for (r = reject; *r != '\0'; ++r) {
 			if (*p == *r)
 				return count;
--- str/strcspn.h
+++ str/strcspn.h
@@ -2,7 +2,31 @@
 #define __STRCSPN_H__
 
 #include "kernel_definitions.h"
+#include "strlen.h"
 
+/*@ requires valid_str(s);
+    requires valid_str(reject);
+    assigns \nothing;
+    ensures 0 <= \result <= strlen(s);
+    ensures \forall char *p, *t;
+            s <= p < s + \result &&
+            reject <= t < reject + strlen(reject) ==>
+            *p != *t;
+    behavior exists:
+       assumes \exists char *p, *t;
+               s <= p < s + strlen(s) &&
+               reject <= t < reject + strlen(reject) &&
+               *p == *t;
+       ensures \exists char *t; reject <= t < reject + strlen(reject) && s[\result] == *t;
+    behavior not_exists:
+       assumes \forall char *p, *t;
+               s <= p < s + strlen(s) &&
+               reject <= t < reject + strlen(reject) ==>
+               *p != *t;
+       ensures \result == strlen(s);
+    complete behaviors;
+    disjoint behaviors;
+ */
 size_t strcspn(const char *s, const char *reject);
 
 #endif // __STRCSPN_H__
