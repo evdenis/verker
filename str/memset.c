9f072b9a1ad94b41d2e713f9d502d2a3ef82dee2
Denis Efremov <yefremov.denis@gmail.com>
2017-05-09 14:06:13 +0300

    memset added

---
+++ str/memset.c
@@ -0,0 +1,10 @@
+#include "memset.h"
+
+void *memset(void *s, int c, size_t count)
+{
+	char *xs = s;
+
+	while (count--)
+		*xs++ = c;
+	return s;
+}
---
+++ str/memset.h
@@ -0,0 +1,17 @@
+#ifndef __MEMSET_H__
+#define __MEMSET_H__
+
+#include "kernel_definitions.h"
+
+/**
+ * memset - Fill a region of memory with the given value
+ * @s: Pointer to the start of the area.
+ * @c: The byte to fill the area with
+ * @count: The size of the area.
+ *
+ * Do not use memset() to access IO space, use memset_io() instead.
+ */
+
+void *memset(void *s, int c, size_t count);
+
+#endif // __MEMSET_H__
