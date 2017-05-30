ca9e126b8e3654b1d1e35ac0d47365a229de4e25
Denis Efremov <efremov@ispras.ru>
2017-05-30 20:01:39 +0300

    strlen update

--- strlen.c
+++ strlen.c
@@ -3,14 +3,23 @@
 size_t strlen(const char *s)
 {
 	const char *sc;
-	/*@ loop invariant \base_addr(s) == \base_addr(sc);
-	    loop invariant s <= sc <= s + strlen(s);
+	/*@ loop invariant s <= sc <= s + strlen(s);
 	    loop invariant valid_str(sc);
 	    loop invariant strlen(s) == strlen(sc) + (sc - s);
 	    loop variant strlen(s) - (sc - s);
-	    //loop variant strlen(sc);
 	 */
 	for (sc = s; *sc != '\0'; ++sc)
 		/* nothing */;
 	return sc - s;
 }
+
+#ifdef OUT_OF_TASK
+
+int LLVMFuzzerTestOneInput(uint8_t *data,
+                           size_t size)
+{
+    data[size - 1] = '\0';
+    strlen(data);
+    return 0;
+}
+#endif
