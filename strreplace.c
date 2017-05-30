e2e8abd4932b0544c55c0872cd4bac78ed3b0d57
Denis Efremov <efremov@ispras.ru>
2017-05-30 20:02:50 +0300

    strreplace update

--- strreplace.c
+++ strreplace.c
@@ -11,14 +11,15 @@ char *strreplace(char *s, char old, char new)
 {
 	//@ ghost char *os = s;
 	/*@ loop invariant valid_str(s);
+	    loop invariant strlen(os) == strlen(s) + (os - s);
 	    loop invariant os <= s <= os + strlen(os);
-	    //loop invariant \forall char *p;
-	    //   os <= p < s &&
-	    //   \at(*p,Pre) != old ==> *p == \at(*p,Pre);
 	    loop invariant \forall char *p;
 	       os <= p < s && \at(*p,Pre) == old ==>
 	          *p == new;
 	    loop invariant \forall char *p; s <= p <= os + strlen(os) ==> \at(*p,Pre) == *p;
+	    loop invariant \forall char *p;
+	       os <= p < os + strlen(os) && \at(*p,Pre) != old ==>
+	          \at(*p,Pre) == *p;
 	    loop assigns os[0..strlen(os)];
 	    loop variant strlen(os) - (s - os);
 	 */
