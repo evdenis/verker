9a333d72e8f4aee201fa3eb2e2ad585a9458c907
Denis Efremov <efremov@ispras.ru>
2017-07-10 21:10:56 +0300

    match_string specification update

--- match_string.c
+++ match_string.c
@@ -1,28 +1,38 @@
 #include "match_string.h"
 
+/*@ predicate null_terminator(char **array) =
+       \exists integer i;
+          0 <= i &&
+          array[i] == \null &&
+          (\forall integer j; 0 <= j < i ==> array[j] != \null);
+    logic integer null_at(char **array) =
+       *array == \null ? 1 : 1 + null_at(array + 1);
+ */
 
 /*@ requires valid_str(string);
     requires n <= INT_MAX;
-    requires \valid(array + (0 .. n - 1));
-    requires \forall integer i; 0 <= i < n - 1 ==> valid_str(array[i]);
-    requires array[n - 1] == \null || valid_str(array[n - 1]);
+    requires (null_terminator(array) &&
+             null_at(array) <= n     &&
+             \valid(array + (0 .. null_at(array))) &&
+             (\forall integer i; 0 <= i < null_at(array) ==> valid_str(array[i])))
+             ||
+             (\valid(array + (0 .. n - 1)) &&
+             (\forall integer i; 0 <= i < n ==> valid_str(array[i])));
     assigns \nothing;
-    //behavior b1:
-    //behavior b2:
-    //complete behaviors;
-    //disjoint behaviors;
  */
 int match_string(const char * const *array, size_t n, const char *string)
 {
-	//int index; CODE_CHANGE:
 	int index;
 	const char *item;
 
-	/*@ loop invariant 0 <= index <= n;
+	/*@ loop invariant 0 <= index;
+	    loop invariant null_terminator(array) ==> index <= null_at(array);
+	    loop invariant !null_terminator(array) ==> index <= n;
 	    loop variant n - index;
 	 */
 	for (index = 0; index < n; index++) {
 		item = array[index];
+		//@ assert item == \null || valid_str(item);
 		if (!item)
 			break;
 		if (!strcmp(item, string))
