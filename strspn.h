1847db67439543eb087fb8d0b8880ebc2a3cb6ef
Mikhail Mandrykin <mandrykin@ispras.ru>
2018-08-03 21:24:08 +0300

    Ported strspn to lemma functions, switched to new naming for *.jessie folders, now only separate replay is relevant due to extra global lemmas added by lemma functions

--- strspn.h
+++ strspn.h
@@ -13,49 +13,135 @@
 /*@ axiomatic StrSpn {
     predicate in_array(char *s, char c) = \exists char *p; s <= p < s + strlen(s) && *p == c;
 
-    lemma in_array_shift1:
-       \forall char *s, c;
-          valid_str(s) && s != '\0' && *s != c ==>
-             in_array(s, c) <==> in_array(s + 1, c);
-    lemma in_array_true:
-       \forall char *s, c;
-          valid_str(s) && s != '\0' && *s == c ==>
-             in_array(s, c);
-    lemma in_array_false:
-       \forall char *s, c;
-          valid_str(s) && s == '\0' ==>
-             !in_array(s, c);
+    //lemma in_array_shift1:
+    //   \forall char *s, c;
+    //     valid_str(s) && s != '\0' && *s != c ==>
+    //         in_array(s, c) <==> in_array(s + 1, c);
+    //lemma in_array_true:
+    //   \forall char *s, c;
+    //      valid_str(s) && s != '\0' && *s == c ==>
+    //         in_array(s, c);
+    //lemma in_array_false:
+    //   \forall char *s, c;
+    //      valid_str(s) && s == '\0' ==>
+    //        !in_array(s, c);
 
-    logic integer strspn(char *s, char *accept);
+    logic integer strspn(char *s, char *accept) =
+      *s == '\0' || ! in_array(accept, *s) ? 0 : 1 + strspn(s + 1, accept);
 
-    lemma strspn_strend:
-       \forall char *s, *accept;
-          \valid(s) && *s == '\0' ==>
-             strspn(s, accept) == 0;
+    //lemma strspn_strend:
+    //   \forall char *s, *accept;
+    //      \valid(s) && *s == '\0' ==>
+    //         strspn(s, accept) == 0;
 
-    lemma strspn_empty_accept:
-       \forall char *s, *accept;
-          \valid(accept) && *accept == '\0' ==>
-             strspn(s, accept) == 0;
+    //lemma strspn_empty_accept:
+    //   \forall char *s, *accept;
+    //      \valid(accept) && *accept == '\0' ==>
+    //         strspn(s, accept) == 0;
 
-    lemma strspn_range:
-       \forall char* s, *accept;
-          valid_str(s) && valid_str(accept) ==>
-             0 <= strspn(s, accept) <= strlen(s);
+    //lemma strspn_range:
+    //   \forall char* s, *accept;
+    //      valid_str(s) && valid_str(accept) ==>
+    //         0 <= strspn(s, accept) <= strlen(s);
 
-    lemma strspn_shift1:
-       \forall char *s, *accept;
-          valid_str(s) && valid_str(accept) && *s != '\0' &&
-          in_array(accept, *s) ==>
-             strspn(s, accept) == strspn(s + 1, accept) + 1;
+    //lemma strspn_shift1:
+    //   \forall char *s, *accept;
+    //      valid_str(s) && valid_str(accept) && *s != '\0' &&
+    //      in_array(accept, *s) ==>
+    //         strspn(s, accept) == strspn(s + 1, accept) + 1;
 
-    lemma strspn_stop_not_in_accept:
-       \forall char *s, *accept;
-          valid_str(s) && valid_str(accept) && *s != '\0' &&
-          !in_array(accept, *s) ==>
-             strspn(s, accept) == 0;
+    //lemma strspn_stop_not_in_accept:
+    //   \forall char *s, *accept;
+    //      valid_str(s) && valid_str(accept) && *s != '\0' &&
+    //      !in_array(accept, *s) ==>
+    //         strspn(s, accept) == 0;
     }
  */
+
+/*@ ghost
+  @ /@ lemma
+  @  @ requires valid_str(s);
+  @  @ requires *s != '\0';
+  @  @ requires *s != c;
+  @  @ ensures  in_array(s, c) <==> in_array(s + 1, c);
+  @  @/
+  @  void in_array_shift1(char *s, char c)
+  @  {
+  @  }
+  @*/
+
+/*@ ghost
+  @ /@ lemma
+  @  @ requires valid_str(s);
+  @  @ requires *s != '\0';
+  @  @ requires *s == c;
+  @  @ ensures  in_array(s, c);
+  @  @/
+  @  void in_array_true(char *s, char c)
+  @  {
+  @  }
+  @*/
+
+/*@ ghost
+  @ /@ lemma
+  @  @ requires valid_str(s);
+  @  @ requires *s == '\0';
+  @  @ ensures  !in_array(s, c);
+  @  @/
+  @  void in_array_false(char *s, char c)
+  @  {
+  @  }
+  @*/
+
+/*@ ghost
+  @ /@ lemma
+  @  @ requires \valid(s);
+  @  @ requires *s == '\0';
+  @  @ ensures strspn(s, accept) == 0;
+  @  @/
+  @ void strspn_strend(char *s, char *accept)
+  @ {
+  @ }
+  @*/
+
+/*@ ghost
+  @ /@ lemma
+  @  @ requires \valid(accept);
+  @  @ requires *accept == '\0';
+  @  @ ensures strspn(s, accept) == 0;
+  @  @/
+  @ void strspn_empty_accept(char *s, char *accept)
+  @ {
+  @ }
+  @*/
+
+/*@ ghost
+  @ /@ requires valid_str(s);
+  @  @ decreases strlen(s);
+  @  @ ensures \result != 0 <==> in_array(s, c);
+  @  @/
+  @ int in_array(char *s, char c)
+  @ {
+  @   if (*s == '\0') return 0;
+  @   if (*s == c) return 1;
+  @   return in_array(s + 1, c);
+  @ }
+  @*/
+
+/*@ ghost
+  @ /@ lemma
+  @  @ requires valid_str(s) ;
+  @  @ requires valid_str(accept);
+  @  @ decreases strlen(s);
+  @  @ ensures 0 <= strspn(s, accept) <= strlen(s);
+  @  @/
+  @ void strspn_range(char *s, char *accept)
+  @ {
+  @   if (*s != '\0' && in_array(accept, *s)) {
+  @     strspn_range(s + 1, accept);
+  @   }
+  @ }
+  @*/
 
 /*@ requires valid_str(s);
     requires valid_str(accept);
