59caabf4d5b313e9cae993c2279c4b3fd8d4c1e2
Mikhail Mandrykin <mandrykin@ispras.ru>
2018-08-10 22:34:31 +0300

    Ported skip_spaces.h to lemma functions

--- skip_spaces.h
+++ skip_spaces.h
@@ -7,40 +7,96 @@
 
 
 /*@ axiomatic SkipSpaces {
-    logic char *skip_spaces(char *str) ;//=
-       //isspace(*str) ? skip_spaces(str + 1) : str;
-    lemma skip_spaces_defn{L}:
-       \forall char *str, size_t i;
-       valid_str(str) && i <= strlen(str) &&
-       (\forall size_t j; j < i ==> isspace(str[j])) &&
-       !isspace(str[i]) ==>
-          str + i == skip_spaces(str);
-    lemma skip_spaces_deref:
-       \forall char *str; valid_str(str) ==>
-          !isspace(*skip_spaces(str));
-    lemma skip_spaces_range:
-       \forall char *str;
-       valid_str(str) ==>
-          str <= skip_spaces(str) <= str + strlen(str);
-    lemma skip_spaces_iter_one:
-       \forall char *str;
-       valid_str(str) && !isspace(*str) ==>
-       skip_spaces(str) == skip_spaces(str+1);
-    lemma skip_spaces_base_addr:
-       \forall char *str;
-       valid_str(str) ==>
-          \base_addr(str) == \base_addr(skip_spaces(str));
-    lemma skip_spaces_same:
-       \forall char *str;
-       \valid(str) && !isspace(*str) ==>
-          str == skip_spaces(str);
-    lemma skipped_are_spaces:
-       \forall char *str, size_t i;
-       valid_str(str) &&
-       i < skip_spaces(str) - str ==>
-          isspace(str[i]);
+    logic char *skip_spaces(char *str) =
+       isspace(*str) ? skip_spaces(str + 1) : str;
+//    lemma skip_spaces_defn{L}:
+//       \forall char *str, size_t i;
+//       valid_str(str) && i <= strlen(str) &&
+//       (\forall size_t j; j < i ==> isspace(str[j])) &&
+//       !isspace(str[i]) ==>
+//          str + i == skip_spaces(str);
+//    lemma skip_spaces_deref:
+//       \forall char *str; valid_str(str) ==>
+//          !isspace(*skip_spaces(str));
+//    lemma skip_spaces_range:
+//       \forall char *str;
+//       valid_str(str) ==>
+//          str <= skip_spaces(str) <= str + strlen(str);
+//    lemma skip_spaces_iter_one:
+//       \forall char *str;
+//       valid_str(str) && !isspace(*str) ==>
+//       skip_spaces(str) == skip_spaces(str+1);
+//    lemma skip_spaces_base_addr:
+//       \forall char *str;
+//       valid_str(str) ==>
+//          \base_addr(str) == \base_addr(skip_spaces(str));
+//    lemma skip_spaces_same:
+//       \forall char *str;
+//       \valid(str) && !isspace(*str) ==>
+//          str == skip_spaces(str);
+//    lemma skipped_are_spaces:
+//       \forall char *str, size_t i;
+//       valid_str(str) &&
+//       i < skip_spaces(str) - str ==>
+//          isspace(str[i]);
     }
  */
+
+/*@ ghost
+  @ //@ ensures \result <==> isspace(c);
+  @ int is_space(char c)
+  @ {
+  @   return c == ' '  || c == '\f' || c == '\n'
+  @      ||  c == '\r' || c == '\t' || c == '\v';
+  @ }
+  @*/
+
+/*@ ghost
+  @ /@ lemma
+  @  @ requires  valid_str(str);
+  @  @ requires  str <= res <= str + strlen(str);
+  @  @ requires  \forall char *p; str <= p < res ==> isspace(*p);
+  @  @ requires  !isspace(*res);
+  @  @ decreases strlen(str);
+  @  @ ensures   skip_spaces(str) == res;
+  @  @/
+  @ void skip_spaces_defn(char *str, char* res)
+  @ {
+  @    if (isspace(*str)) {
+  @      //@ assert str <= res || str == res;
+  @      //@ assert str + strlen(str) == str + 1 + strlen(str + 1);
+  @      skip_spaces_defn(str + 1, res);
+  @    }
+  @ }
+  @*/
+
+/*@ ghost
+  @ /@ lemma
+  @  @ requires  valid_str(str);
+  @  @ decreases strlen(str);
+  @  @ ensures   str <= skip_spaces(str) <= str + strlen(str);
+  @  @/
+  @ void skip_spaces_range(char *str)
+  @ {
+  @    if (isspace(*str)) {
+  @      skip_spaces_range(str + 1);
+  @    }
+  @ }
+  @*/
+
+/*@ ghost
+  @ /@ lemma
+  @  @ requires  valid_str(str);
+  @  @ requires  i < skip_spaces(str) - str;
+  @  @ decreases i;
+  @  @ ensures   isspace(str[i]);
+  @  @/
+  @ void skipped_are_spaces(char *str, size_t i)
+  @ {
+  @   if (i > 0)
+  @    skipped_are_spaces(str + 1, i - 1);
+  @ }
+  @*/
 
 /**
  * skip_spaces - Removes leading whitespace from @str.
