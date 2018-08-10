577c5038e69e652953f03758ddae6c95e28e47d4
Mikhail Mandrykin <mandrykin@ispras.ru>
2018-08-10 18:07:47 +0300

    Ported strcspn.h to lemma functions

--- strcspn.h
+++ strcspn.h
@@ -12,36 +12,78 @@
  */
 
 /*@ axiomatic StrCSpn {
-    logic integer strcspn(char *s, char *reject);
+    logic integer strcspn(char *s, char *reject) =
+      *s == '\0' || in_array(reject, *s) ? 0 : 1 + strcspn(s + 1, reject);
 
-    lemma strcspn_strend:
-       \forall char *s, *reject;
-          \valid(s) && *s == '\0' ==>
-             strcspn(s, reject) == 0;
+//    lemma strcspn_strend:
+//       \forall char *s, *reject;
+//          \valid(s) && *s == '\0' ==>
+//             strcspn(s, reject) == 0;
 
-    lemma strcspn_empty_reject:
-       \forall char *s, *reject;
-          valid_str(s) && valid_str(reject) && *reject == '\0' ==>
-             strcspn(s, reject) == strlen(s);
+//    lemma strcspn_empty_reject:
+//       \forall char *s, *reject;
+//          valid_str(s) && valid_str(reject) && *reject == '\0' ==>
+//             strcspn(s, reject) == strlen(s);
 
-    lemma strcspn_range:
-       \forall char *s, *reject;
-          valid_str(s) && valid_str(reject) ==>
-             0 <= strcspn(s, reject) <= strlen(s);
+//    lemma strcspn_range:
+//       \forall char *s, *reject;
+//          valid_str(s) && valid_str(reject) ==>
+//             0 <= strcspn(s, reject) <= strlen(s);
 
-    lemma strcspn_shift1:
-       \forall char *s, *reject;
-          valid_str(s) && valid_str(reject) && *s != '\0' &&
-          !in_array(reject, *s) ==>
-             strcspn(s, reject) == strcspn(s + 1, reject) + 1;
+//    lemma strcspn_shift1:
+//       \forall char *s, *reject;
+//          valid_str(s) && valid_str(reject) && *s != '\0' &&
+//          !in_array(reject, *s) ==>
+//             strcspn(s, reject) == strcspn(s + 1, reject) + 1;
 
-    lemma strcspn_stop_in_reject:
-       \forall char *s, *reject;
-          valid_str(s) && valid_str(reject) && *s != '\0' &&
-          in_array(reject, *s) ==>
-             strcspn(s, reject) == 0;
+//    lemma strcspn_stop_in_reject:
+//       \forall char *s, *reject;
+//          valid_str(s) && valid_str(reject) && *s != '\0' &&
+//          in_array(reject, *s) ==>
+//             strcspn(s, reject) == 0;
     }
  */
+
+/*@ ghost
+  @ /@ lemma
+  @  @ requires \valid(s);
+  @  @ requires *s == '\0';
+  @  @ ensures strcspn(s, reject) == 0;
+  @  @/
+  @ void strcspn_strend(char *s, char *reject)
+  @ {
+  @ }
+  @*/
+
+/*@ ghost
+  @ /@ lemma
+  @  @ requires valid_str(s);
+  @  @ requires \valid(reject);
+  @  @ requires *reject == '\0';
+  @  @ decreases strlen(s);
+  @  @ ensures strcspn(s, reject) == strlen(s);
+  @  @/
+  @ void strcspn_empty_accept(char *s, char *reject)
+  @ {
+  @  if (*s != '\0')
+  @    strcspn_empty_accept(s + 1, reject);
+  @ }
+  @*/
+
+/*@ ghost
+  @ /@ lemma
+  @  @ requires valid_str(s) ;
+  @  @ requires valid_str(reject);
+  @  @ decreases strlen(s);
+  @  @ ensures 0 <= strcspn(s, reject) <= strlen(s);
+  @  @/
+  @ void strcspn_range(char *s, char *reject)
+  @ {
+  @   if (*s != '\0' && !in_array(reject, *s)) {
+  @     strcspn_range(s + 1, reject);
+  @   }
+  @ }
+  @*/
 
 /*@ requires valid_str(s);
     requires valid_str(reject);
