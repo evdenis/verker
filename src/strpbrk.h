d6c3de27333183f03e2f878d25fa0ec223feaa38
Denis Efremov <efremov@linux.com>
2019-05-22 09:33:52 +0300

    strpbrk: add lemma functions

--- src/strpbrk.h
+++ src/strpbrk.h
@@ -5,8 +5,11 @@
 #include "strlen.h"
 #include "strspn.h"
 
+#ifndef LEMMA_FUNCTIONS
+
 /*@ axiomatic StrPBrk {
-    logic char *strpbrk{L}(char *cs, char *ct);
+    logic char *strpbrk{L}(char *s, char *ct) =
+      *s == '\0' ? (char*)\null : in_array(ct, *s) ? s : strpbrk(s + 1, ct);
 
     lemma strpbrk_strend:
        \forall char *cs, *ct;
@@ -37,6 +40,51 @@
              strpbrk(cs, ct) == cs;
     }
  */
+
+#else
+
+/*@ axiomatic StrPBrk {
+    logic char *strpbrk{L}(char *s, char *ct) =
+      *s == '\0' ? (char*)\null : in_array(ct, *s) ? s : strpbrk(s + 1, ct);
+    }
+ */
+
+/*@ ghost
+  @ /@ lemma
+  @  @ requires  valid_str(s);
+  @  @ requires  valid_str(ct);
+  @  @ decreases strlen(s);
+  @  @ ensures   strpbrk(s, ct) == \null ^^ s <= strpbrk(s, ct) < s + strlen(s);
+  @  @/
+  @ void strpbrk_range(char *s, char *ct)
+  @ {
+  @   if (*s != '\0' && !in_array(ct, *s))
+  @     strpbrk_range(s + 1, ct);
+  @ }
+  @*/
+
+/*@ ghost
+  @ /@ lemma
+  @  @ requires  valid_str(s1);
+  @  @ requires  valid_str(s2);
+  @  @ requires  strlen(s2) >= strlen(s1);
+  @  @ requires  valid_str(ct);
+  @  @ requires  strpbrk(s1, ct) != \null;
+  @  @ requires  \forall char *s; s2 <= s <= s2 + (strpbrk(s1, ct) - s1) ==> *s == s1[s - s2];
+  @  @ decreases strlen(s1);
+  @  @ ensures   strpbrk(s2, ct) != \null;
+  @  @ ensures   strpbrk(s2, ct) - s2 == strpbrk(s1, ct) - s1;
+  @  @/
+  @ void strpbrk_frame(char *s1, char *s2, char *ct) {
+  @   if (*s1 != '\0') {
+  @     if (!in_array(ct, *s1)) {
+  @       strpbrk_frame(s1 + 1, s2 + 1, ct);
+  @     }
+  @   }
+  @ }
+  @*/
+
+#endif /* LEMMA_FUNCTIONS */
 
 /**
  * strpbrk - Find the first occurrence of a set of characters
