42224a580947a58f1b6e83a34da35e03d2cf60b2
Denis Efremov <efremov@linux.com>
2019-05-22 08:39:24 +0300

    strchr: add lemma_functions

--- src/strchr.h
+++ src/strchr.h
@@ -4,6 +4,8 @@
 #include "kernel_definitions.h"
 #include "strlen.h"
 
+#ifndef LEMMA_FUNCTIONS
+
 /*@ axiomatic Strchr {
     logic char *strchr(char *str, char c) =
        *str == c ? str : ((*str == '\0') ? (char *) \null : strchr(str+1, c));
@@ -43,6 +45,91 @@
           str[i] != c;
     }
  */
+
+#else
+
+/*@ axiomatic Strchr {
+    logic char *strchr(char *str, char c) =
+       *str == c ? str : ((*str == '\0') ? (char *) \null : strchr(str+1, c));
+    }
+ */
+
+/*@ ghost
+  @ /@ lemma
+  @  @ requires  valid_str(str);
+  @  @ decreases strlen(str);
+  @  @ ensures   (str <= strchr(str, c) <= str + strlen(str)) ^^
+  @  @           strchr(str, c) == \null;
+  @  @/
+  @ void strchr_mem(char *str, char c)
+  @ {
+  @    if (*str != c && *str != '\0')
+  @      strchr_mem(str + 1, c);
+  @ }
+  @*/
+
+/*@ ghost
+  @ /@ lemma
+  @  @ requires  valid_str(str);
+  @  @ requires  c != '\0';
+  @  @ decreases strlen(str);
+  @  @ ensures   strchr(str, c) == \null ^^
+  @  @           (\valid(strchr(str,c)) && *strchr(str, c) == c);
+  @  @/
+  @ void strchr_res(char *str, char c)
+  @ {
+  @    if (*str != c && *str != '\0')
+  @      strchr_res(str + 1, c);
+  @ }
+  @*/
+
+/*@ ghost
+  @ /@ lemma
+  @  @ requires  valid_str(str);
+  @  @ requires  0 <= i <= strlen(str);
+  @  @ requires  \forall integer j; 0 <= j < i ==> str[j] != c;
+  @  @ requires  str[i] == c;
+  @  @ decreases strlen(str);
+  @  @ ensures   strchr(str, c) == str + i;
+  @  @/
+  @ void strchr_defn(char *str, char c, size_t i)
+  @ {
+  @    if (*str != c && *str != '\0')
+  @      strchr_defn(str + 1, c, i - 1);
+  @ }
+  @*/
+
+/*@ ghost
+  @ /@ lemma
+  @  @ requires  valid_str(str);
+  @  @ requires  \exists char * p; str <= p < str + strlen(str) && *p == c;
+  @  @ decreases strlen(str);
+  @  @ ensures   strchr(str, c) != \null && \base_addr(strchr(str, c)) == \base_addr(str);
+  @  @/
+  @ void strchr_not_null(char *str, char c)
+  @ {
+  @   if (*str != c && *str != '\0')
+  @     strchr_not_null(str + 1, c);
+  @ }
+  @*/
+
+/*@ ghost
+  @ /@ lemma
+  @  @ requires  valid_str(str);
+  @  @ requires  strchr(str, c) != \null;
+  @  @ requires  0 <= i < strchr(str, c) - str <= strlen(str);
+  @  @ decreases i;
+  @  @ ensures   str[i] != c;
+  @  @/
+  @ void strchr_skipped(char *str, char c, size_t i)
+  @ {
+  @   if (i > 0 && *str != '\0' && *str != c) {
+  @     strchr_skipped(str + 1, c, i - 1);
+  @  }
+  @ }
+  @*/
+
+#endif /* LEMMA_FUNCTIONS */
 
 /**
  * strchr - Find the first occurrence of a character in a string
