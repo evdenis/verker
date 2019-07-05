25d2856af3283c2d9ae3d6daf2325c59a68dc8c1
Igor Tarakanov <igortarakanov@mail.ru>
2019-07-05 15:50:19 +0300

    [+] match_string proved

--- src/match_string.c
+++ src/match_string.c
@@ -1,39 +1,23 @@
 #include "match_string.h"
 
-/*@ predicate null_terminator(char **array) =
-       \exists integer i;
-          0 <= i &&
-          array[i] == \null &&
-          (\forall integer j; 0 <= j < i ==> array[j] != \null);
-    logic integer null_at(char **array) =
-       *array == \null ? 1 : 1 + null_at(array + 1);
- */
 
-/*@ requires valid_str(string);
-    requires n <= INT_MAX;
-    requires (null_terminator(array) &&
-             null_at(array) <= n     &&
-             \valid_read(array + (0 .. null_at(array))) &&
-             (\forall integer i; 0 <= i < null_at(array) ==> valid_str(array[i])))
-             ||
-             (\valid_read(array + (0 .. n - 1)) &&
-             (\forall integer i; 0 <= i < n ==> valid_str(array[i])));
-    assigns \nothing;
- */
 int match_string(const char * const *array, size_t n, const char *string)
 {
 	int index;
 	const char *item;
 
-	/*@ loop invariant 0 <= index;
-	    loop invariant null_terminator(array) ==> index <= null_at(array);
-	    loop invariant !null_terminator(array) ==> index <= n;
-	    loop assigns index;
-	    loop variant n - index;
-	 */
+    /*@
+        loop invariant 0 <= index <= n;
+        loop invariant \forall integer k; 0 <= k < index ==>
+            strcmp(array[k], string) != 0;
+		loop invariant \forall integer k; 0 <= k < index ==>
+            array[k] != NULL;
+        loop assigns index;
+		loop assigns item;
+        loop variant n - index;
+    */
 	for (index = 0; index < n; index++) {
 		item = array[index];
-		//@ assert item == \null || valid_str(item);
 		if (!item)
 			break;
 		if (!strcmp(item, string))
@@ -42,25 +26,3 @@ int match_string(const char * const *array, size_t n, const char *string)
 
 	return -EINVAL;
 }
-
-
-#ifdef DUMMY_MAIN
-
-int main(int argc, char *argv[])
-{
-	const char *str = "12345";
-	const char *list[] = {
-		"TEST",
-		"TST",
-		"TS",
-		"T",
-		"12345",
-		NULL
-	};
-
-	match_string(list, -1, str);
-	match_string(list, 3, str);
-
-	return 0;
-}
-#endif
--- src/match_string.h
+++ src/match_string.h
@@ -1,9 +1,7 @@
 #ifndef __MATCH_STRING_H__
 #define __MATCH_STRING_H__
 
-#include "kernel_definitions.h"
 #include "strcmp.h"
-
 
 /**
  * match_string - matches given string in an array
@@ -15,6 +13,38 @@
  * index of a @string in the @array if matches, or %-EINVAL otherwise.
  */
 
+/*@
+    logic integer cmp(unsigned char a, unsigned char b) =
+       a == b ? 0 : a < b ? -1 : 1;
+
+    logic integer strncmp(char *cs, char *ct, integer n) =
+       n == -1 ? 0 : (cs[n] == ct[n] ? strncmp(cs+1, ct+1, n-1) : cmp((u8 AENO)cs[n], (u8 AENO)ct[n]));
+    logic integer strcmp(char *cs, char *ct) = strncmp(cs, ct, strlen(cs));
+*/
+
+/*@
+    requires \valid_read(array + (0..n-1));
+    requires valid_str(string);
+    requires n <= INT_MAX;
+    requires \forall integer i; 0 <= i < n && valid_str(array[i]);
+
+    assigns   \nothing;
+
+    behavior exists:
+        assumes  \exists integer i; 0 <= i < n && strcmp(array[i], string) == 0;
+        ensures  0 <= \result < n;
+        ensures  strcmp(array[\result], string) == 0;
+        ensures  \forall integer i; 0 <= i < \result ==>
+            strcmp(array[\result], string) != 0;
+
+    behavior missing:
+        assumes  \forall integer i; 0 <= i < n ==>
+            strcmp(array[i], string) != 0;
+        ensures  \result == -EINVAL;
+
+    complete behaviors;
+    disjoint behaviors;
+*/
 int match_string(const char * const *array, size_t n, const char *string);
 
 #endif // __MATCH_STRING_H__
