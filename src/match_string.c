baa74c107106556708dd2ee97c235bac1c969082
Igor Tarakanov <igortarakanov@mail.ru>
2019-07-08 14:10:07 +0300

    [wip]

--- src/match_string.c
+++ src/match_string.c
@@ -6,25 +6,30 @@ int match_string(const char * const *array, size_t n, const char *string)
 	int index;
 	const char *item;
 
-	/*@ assert 0 == 1; */
 
     /*@
-        loop invariant 0 <= index <= n;
-        loop invariant \forall integer k; 0 <= k < index ==>
-            strcmp(array[k], string) != 0;
-		loop invariant \forall integer k; 0 <= k < index ==>
-            array[k] != NULL;
+        loop invariant 0 <= index < real_len(array, n);
+        loop invariant \forall integer k; 0 <= k < index ==> (
+            array[k] != NULL &&
+            strcmp(array[k], string) != 0
+		);
         loop assigns index;
 		loop assigns item;
-        loop variant n - index;
+        loop variant real_len(array, n) - index;
     */
+
 	for (index = 0; index < n; index++) {
 		item = array[index];
 		if (!item)
 			break;
+		//@ assert array[index] != NULL;
 		if (!strcmp(item, string))
 			return index;
+		//@ assert strcmp(array[index], string) != 0;
+		//@ assert checked_head(array, string, index);
 	}
+
+	//@ assert checked_head(array, string, n);
 
 	return -EINVAL;
 }
--- src/match_string.h
+++ src/match_string.h
@@ -14,49 +14,130 @@
  * index of a @string in the @array if matches, or %-EINVAL otherwise.
  */
 
+
 /*@
-    logic size_t _real_len(char **array, size_t n) =
-        array[0] == NULL
-        ? (size_t) 0
-        : (
-            n > 0
-            ? (size_t) (1 + _real_len(array + 1, (size_t)(n - 1)))
-            : 0
-        );
+    axiomatic MatchString {
+        logic integer real_len(char **array, integer n) =
+            ((n <= 0) || (array[0] == NULL))
+            ? 0
+            : 1 + real_len(array + 1, n - 1);
+
+        predicate checked_head(char** array, char* string, integer tail) =
+            \forall integer k;
+                (0 <= k < tail) ==> (
+                    array[k] != NULL &&
+                    strcmp(array[k], string) != 0
+                );
+
+        lemma zero_len_a:
+            \forall char** array;
+                \valid_read(array) ==>
+                    real_len(array, 0) == 0;
+        
+        lemma zero_len_b:
+            \forall char** array, integer n;
+                (
+                    \valid_read(array) &&
+                    array[0] == NULL &&
+                    n > 0
+                ) ==>
+                    real_len(array, n) == 0;
+        
+        lemma one_element:
+            \forall char** array;
+                (
+                    \valid_read(array) &&
+                    array[0] != NULL
+                ) ==>
+                    real_len(array, 1) == 1;
+        
+        lemma basic:
+            \forall char** array, integer n;
+                (
+                    n > 0 &&
+                    \valid_read(array+(0..n)) &&
+                    array[0] != NULL
+                ) ==>
+                    (real_len(array, n) ==
+                        1 + real_len(array + 1, n - 1));
+        
+        lemma one_element_or_more:
+            \forall char** array, integer n;
+                (
+                    n > 0 &&
+                    \valid_read(array + (0..n)) &&
+                    array[0] != NULL
+                ) ==>
+                    real_len(array, n + 1) > 0;
+
+        lemma not_null_terminated:
+            \forall char** array, integer n;
+                (
+                    \valid_read(array+(0..n)) &&
+                    (\forall integer k; k <= n ==> array[k] != NULL)
+                ) ==>
+                    real_len(array, n + 1) == n + 1;
+
+        lemma lower_bound:
+            \forall char** array, integer n, char* string;
+                (
+                    valid_str(string) &&
+                    n > 0 &&
+                    \valid_read(array+(0..n))
+                ) ==> (
+                    \forall integer k;
+                        0 <= k <= n &&
+                        checked_head(array, string, k) ==>
+                            real_len(array, n + 1) >= k
+                );
+
+        lemma upper_bound:
+            \forall char** array, integer n;
+                (
+                    n >= 0 &&
+                    \valid_read(array + (0..n))
+                ) ==>
+                    (0 <= real_len(array, n + 1) <= n + 1);
+        
+    }
 */
 
 /*@
-    requires _real_len(array, n) <= SIZE_MAX;
-    requires \valid_read(array + (0.._real_len(array, n)-1));
+    requires real_len(array, n) <= SIZE_MAX;
+    requires \valid_read(array + (0..real_len(array, n)-1));
     requires valid_str(string);
 
-    requires \forall integer i;
-        0 <= i < _real_len(array, n) &&
-        valid_str(array[i]);
+    requires \forall integer k;
+        (0 <= k < real_len(array, n)) ==>
+        valid_str(array[k]);
 
     assigns \nothing;
 
     behavior exists:
-        assumes \exists integer i;
-            0 <= i < _real_len(array, n) &&
-            strcmp(array[i], string) == 0;
+        assumes \exists integer k;
+            (0 <= k < real_len(array, n)) ==>
+            strcmp(array[k], string) == 0;
 
-        ensures 0 <= \result < _real_len(array, n);
+        ensures 0 <= \result < real_len(array, n);
         ensures strcmp(array[\result], string) == 0;
         ensures \forall integer k;
-            0 <= k < \result &&
-            strcmp(array[\result], string) != 0;
+            (0 <= k < \result) ==>
+                (
+                    array[k] != NULL &&
+                    strcmp(array[\result], string) != 0
+                );
 
     behavior missing:
-        assumes \forall integer i;
-            0 <= i < _real_len(array, n) &&
-            strcmp(array[i], string) != 0;
+        assumes \forall integer k;
+            (0 <= k < real_len(array, n)) ==>
+                strcmp(array[k], string) != 0;
 
         ensures \result == -EINVAL;
 
     complete behaviors;
     disjoint behaviors;
 */
+
 int match_string(const char * const *array, size_t n, const char *string);
 
 #endif // __MATCH_STRING_H__
