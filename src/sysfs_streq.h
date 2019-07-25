ae71c3fcf27cc10e76769ec1647a3f2ebb7607e5
Denis Efremov <efremov@linux.com>
2019-07-25 08:46:28 +0300

    sysfs_streq: rename string arguments in specs

--- src/sysfs_streq.h
+++ src/sysfs_streq.h
@@ -17,63 +17,63 @@
 
 
 /*@ axiomatic SysfsStr {
-    logic size_t sysfs_strlen(char* string) =
-       ((string[0] == '\0') ||
-        (string[0] == '\n' && string[1] == '\0')) ?
+    logic size_t sysfs_strlen(char *s) =
+       ((s[0] == '\0') ||
+        (s[0] == '\n' && s[1] == '\0')) ?
         0
 	:
-	(size_t)(1 + sysfs_strlen(string));
+	(size_t)(1 + sysfs_strlen(s));
 
-    logic bool strncmp(char* string1, char* string2, size_t n) =
+    logic bool strncmp(char *s1, char *s2, size_t n) =
        n <= 0 ?
           (bool)true
        :
-          (string1[0] == string2[0] ?
-	     (string1[0] == '\0' ? (bool)true : strncmp(string1 + 1, string2 + 1, (size_t)(n - 1)))
+          (s1[0] == s2[0] ?
+	     (s1[0] == '\0' ? (bool)true : strncmp(s1 + 1, s2 + 1, (size_t)(n - 1)))
           :
 	     (bool)false);
 
     lemma strncmp_shift:
-       \forall char* string1, char* string2, size_t n;
-          valid_str(string1) &&
-          valid_str(string2) &&
+       \forall char *s1, char *s2, size_t n;
+          valid_str(s1) &&
+          valid_str(s2) &&
           n > 0 ==>
-          strncmp(string1, string2, n) ==>
-          strncmp(string1 + 1, string2 + 1, (size_t)(n - 1));
+          strncmp(s1, s2, n) ==>
+          strncmp(s1 + 1, s2 + 1, (size_t)(n - 1));
 
     lemma strncmp_definition:
-       \forall char* string1, char* string2, size_t n;
-          valid_str(string1) &&
-          valid_str(string2) ==>
-          (\forall size_t i; 0 <= i < n ==> string1[i] == string2[i]) ==>
-          strncmp(string1, string2, n);
+       \forall char *s1, char *s2, size_t n;
+          valid_str(s1) &&
+          valid_str(s2) ==>
+          (\forall size_t i; 0 <= i < n ==> s1[i] == s2[i]) ==>
+          strncmp(s1, s2, n);
 
     lemma sysfs_strlen_n_equal:
-       \forall char* string1, char* string2, size_t n;
-          valid_str(string1) &&
-          valid_str(string2) ==>
+       \forall char *s1, char *s2, size_t n;
+          valid_str(s1) &&
+          valid_str(s2) ==>
           (\exists size_t i;
-	     (0 <= i < \min(sysfs_strlen(string1), sysfs_strlen(string2))) &&
-	     (string1[i] != string2[i])) ==>
-          strncmp(string1, string2, (size_t)\min(sysfs_strlen(string1), sysfs_strlen(string2))) == (bool)false;
+	     (0 <= i < \min(sysfs_strlen(s1), sysfs_strlen(s2))) &&
+	     (s1[i] != s2[i])) ==>
+          strncmp(s1, s2, (size_t)\min(sysfs_strlen(s1), sysfs_strlen(s2))) == (bool)false;
 
     lemma sysfs_strlen_bsn:
-       \forall char* string, size_t n;
-          valid_str(string) &&
-          (\forall size_t i; 0 <= i < n ==> string[i] != '\0') &&
-          string[n] == '\n' &&
-          string[n + 1] == '\0' ==>
-          sysfs_strlen(string) == n;
+       \forall char *s, size_t n;
+          valid_str(s) &&
+          (\forall size_t i; 0 <= i < n ==> s[i] != '\0') &&
+          s[n] == '\n' &&
+          s[n + 1] == '\0' ==>
+          sysfs_strlen(s) == n;
 
     lemma sysfs_strlen_equal:
-       \forall char* string1, char* string2, size_t n;
-          valid_str(string1) &&
-          valid_str(string2) &&
-          (\forall size_t i; 0 <= i < n ==> string1[i] == string2[i]) &&
-          string1[n] == '\0' &&
-          string2[n] == '\0' ==>
-	  (sysfs_strlen(string1) == sysfs_strlen(string2) &&
-           strncmp(string1, string2, (size_t)\min(sysfs_strlen(string1), sysfs_strlen(string2))) == (bool)true);
+       \forall char* s1, char* s2, size_t n;
+          valid_str(s1) &&
+          valid_str(s2) &&
+          (\forall size_t i; 0 <= i < n ==> s1[i] == s2[i]) &&
+          s1[n] == '\0' &&
+          s2[n] == '\0' ==>
+	  (sysfs_strlen(s1) == sysfs_strlen(s2) &&
+           strncmp(s1, s2, (size_t)\min(sysfs_strlen(s1), sysfs_strlen(s2))) == (bool)true);
     }
  */
 
