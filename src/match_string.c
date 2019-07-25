3b93699373747d931345c448f57ad1649257bac7
Denis Efremov <efremov@linux.com>
2019-07-25 15:35:58 +0300

    match_string: restore original main

--- src/match_string.c
+++ src/match_string.c
@@ -57,23 +57,24 @@ int match_string(const char * const *array, size_t n, const char *string)
 	return -EINVAL;
 }
 
-#ifdef DUMMY_MAIN	
 
- int main(int argc, char *argv[])	
-{	
-	const char *str = "12345";	
-	const char *list[] = {	
-		"TEST",	
-		"TST",	
-		"TS",	
-		"T",	
-		"12345",	
-		NULL	
-	};	
+#ifdef DUMMY_MAIN
 
- 	match_string(list, -1, str);	
-	match_string(list, 3, str);	
+int main(int argc, char *argv[])
+{
+	const char *str = "12345";
+	const char *list[] = {
+		"TEST",
+		"TST",
+		"TS",
+		"T",
+		"12345",
+		NULL
+	};
 
- 	return 0;	
-}	
+	match_string(list, -1, str);
+	match_string(list, 3, str);
+
+	return 0;
+}
 #endif
